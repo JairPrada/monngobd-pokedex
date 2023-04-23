de7b5532c777591acbadf3058744fd2e4dfcb448
Jair Andres Prada Sierra <jpradas@fundaiongruposocial.co>
2023-04-22 19:28:42 -0500

    feat(src/commons/adapters/http): adding http adapter to do fetchs

---
+++ src/common/adapters/axios.adapter.ts
@@ -0,0 +1,18 @@
+import { HttpAdapter } from "../interfaces/http-adapters.interface";
+import axios, { AxiosInstance } from "axios";
+import { Injectable } from '@nestjs/common';
+
+@Injectable()
+export class AxiosAdapter implements HttpAdapter {
+  private readonly axios: AxiosInstance = axios;
+
+  async get<T>(url: string): Promise<T> {
+    try {
+      const { data } = await this.axios.get<T>(url);
+      return data;
+
+    } catch (error) {
+        throw new Error(error);
+    }
+  }
+}
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,19 +1,37 @@
 import { Injectable } from "@nestjs/common";
-import axios, { AxiosInstance } from "axios";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { Pokemon } from "src/pokemon/entities/pokemon.entity";
+import { AxiosAdapter } from "../common/adapters/axios.adapter";
 
 @Injectable()
 export class SeedService {
-  private readonly axios: AxiosInstance = axios;
+  
+  constructor(
+    @InjectModel(Pokemon.name)
+    private readonly pokemonModel: Model<Pokemon>,
+    private readonly http: AxiosAdapter
+  ) {}
 
   async executeSeed() {
-    const { data } = await this.axios.get<PokeResponse>(
+    await this.pokemonModel.deleteMany({});
+    const data = await this.http.get<PokeResponse>(
       "https://pokeapi.co/api/v2/pokemon?limit=650"
     );
+    const pokemonToInsert: { name: string; no: number }[] = [];
 
     data.results.forEach((pokemon) => {
-      const id = pokemon.url.split("/")[6];
-      
+      const segments = pokemon.url.split("/");
+      const id = +segments[segments.length - 2];
+      pokemonToInsert.push({
+        name: pokemon.name,
+        no: id,
+      });
     });
+
+    console.log(pokemonToInsert);
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
 
     return data.results;
   }
